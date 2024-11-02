b47ab8f052f3e2841c4e5761edcf9d64fe7d7283
AQUA GAMING <kanikabisht020127@gmail.com>
2024-11-02 15:58:00 +0530

    used api key

--- src/api.js
+++ src/api.js
@@ -1,7 +1,8 @@
 // src/api.js
 import axios from 'axios';
 
-const API_KEY = '68170eeb4d91432aab7a49d8fca62cd4';
+// Access the API key from the .env file
+const API_KEY = process.env.REACT_APP_API_KEY;
 const BASE_URL = 'https://newsapi.org/v2';
 
 export const fetchNews = async (category = 'general', page = 1, pageSize = 20) => {
@@ -9,7 +10,7 @@ export const fetchNews = async (category = 'general', page = 1, pageSize = 20) =
     const response = await axios.get(`${BASE_URL}/top-headlines`, {
       params: {
         category,
-        apiKey: API_KEY,
+        apiKey: API_KEY, // Use the API key from the environment variable
         country: 'us', // Change this as needed
         page,          // Page number for pagination
         pageSize,      // Number of articles per page
